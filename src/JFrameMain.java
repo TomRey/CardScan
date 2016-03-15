a8c183cef55d5f57d33f7ad70584f059db63d070
Wirth <wirjy93@gmail.com>
2016-03-15 09:06:45 +0100

    Premier commit - squelette appli + premier test OCR

---
+++ src/JFrameMain.java
@@ -0,0 +1,74 @@
+	import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+	public class JFrameMain extends JFrame
+	{
+
+	/*------------------------------------------------------------------*\
+	|*							Constructeurs							*|
+	\*------------------------------------------------------------------*/
+
+	public JFrameMain()
+		{
+		geometry();
+		control();
+		appearance();
+		}
+
+	/*------------------------------------------------------------------*\
+	|*							Methodes Public							*|
+	\*------------------------------------------------------------------*/
+
+	/*------------------------------*\
+	|*				Set				*|
+	\*------------------------------*/
+
+	/*------------------------------*\
+	|*				Get				*|
+	\*------------------------------*/
+
+	/*------------------------------------------------------------------*\
+	|*							Methodes Private						*|
+	\*------------------------------------------------------------------*/
+
+	private void geometry()
+		{
+		// JComponent : Instanciation
+		panelMain = new JPanelMain();
+		
+		// Layout : Specification
+			{
+			BorderLayout borderLayout=new BorderLayout();
+			setLayout(borderLayout);
+
+			// borderLayout.setHgap(20);
+			// borderLayout.setVgap(20);
+			}
+
+
+			// JComponent : add
+			add(panelMain,BorderLayout.CENTER);
+		}
+
+	private void control()
+		{
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		}
+
+	private void appearance()
+		{
+		setSize(600, 400);
+		setLocationRelativeTo(null); // frame centrer
+		setVisible(true); // last!
+		}
+
+	/*------------------------------------------------------------------*\
+	|*							Attributs Private						*|
+	\*------------------------------------------------------------------*/
+
+	// Tools
+	private JPanelMain panelMain;
+
+
+	}
---
+++ src/JPanelMain.java
@@ -0,0 +1,84 @@
+import java.awt.FlowLayout;
+import java.io.File;
+
+import javax.swing.JPanel;
+
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+public class JPanelMain extends JPanel
+	{
+
+	/*------------------------------------------------------------------*\
+		|*							Constructeurs							*|
+		\*------------------------------------------------------------------*/
+
+	public JPanelMain()
+		{
+		geometry();
+		control();
+		appearance();
+		}
+
+	/*------------------------------------------------------------------*\
+		|*							Methodes Public							*|
+		\*------------------------------------------------------------------*/
+
+	/*------------------------------*\
+		|*				Set				*|
+		\*------------------------------*/
+
+	/*------------------------------*\
+		|*				Get				*|
+		\*------------------------------*/
+
+	/*------------------------------------------------------------------*\
+		|*							Methodes Private						*|
+		\*------------------------------------------------------------------*/
+
+	private void geometry()
+		{
+		// JComponent : Instanciation
+
+		// Layout : Specification
+			{
+			FlowLayout flowlayout = new FlowLayout(FlowLayout.CENTER);
+			setLayout(flowlayout);
+
+			// flowlayout.setHgap(20);
+			// flowlayout.setVgap(20);
+			}
+
+		// JComponent : add
+
+		}
+
+	private void control()
+		{
+		File imageFile = new File("C:/Users/Jeremy/Pictures/nomnum.png");
+		//File imageFile = new File("eurotext.bmp");
+        ITesseract instance = new Tesseract();  // JNA Interface Mapping
+        //ITesseract instance = new Tesseract1(); // JNA Direct Mapping
+
+        try {
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+        } catch (TesseractException e) {
+            System.out.println("ERREEUR");
+        }
+
+		}
+
+	private void appearance()
+		{
+		// rien
+		}
+
+	/*------------------------------------------------------------------*\
+		|*							Attributs Private						*|
+		\*------------------------------------------------------------------*/
+
+	// Tools
+
+	}
